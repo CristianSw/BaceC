a7a5524174e21ff46b64d608d9a04ee1f42d0b3b
CristianSw <cristian.sw@protonmail.com>
2022-11-09 10:15:26 +0000

    scanf working and syntax

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+
+    char str[100];
+    int integer;
+    float floatValue;
+    double doubleValue;
+
+//    printf("Hello, Cristian remind C!\n");
+    printf("Enter some string : ");
+    scanf("%s", str);
+    printf("Enter some int : ");
+    scanf("%d", &integer);
+    printf("Enter some float : ");
+    scanf("%f", &floatValue);
+    printf("Enter some double : ");
+    scanf("%lf", &doubleValue);
+    printf("inserted : %s------%d------%f------%lf", str, integer,floatValue,doubleValue);
     return 0;
 }
